8ebd1911c399ca65932384885b10103f0e450e47
slmutli <108276889+slmulti@users.noreply.github.com>
2023-03-06 10:51:40 +0000

    styling

--- src/supabase.js
+++ src/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://smrymzgcbdfnqqgjzwpw.supabase.co";
 const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtcnltemdjYmRmbnFxZ2p6d3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc4NDMyMjcsImV4cCI6MTk5MzQxOTIyN30.g-nQvd0eiX6n_vEiAmU1HM35TnzidpA0Zi2VILQ5-uM";
+
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
